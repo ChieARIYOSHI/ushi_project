a825fd401172362b7d67b10044454a908f0fcaca
ChieARIYOSHI <chic.720ants@gmail.com>
2021-04-21 18:41:02 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Home from './components/home.js';
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+      <p>パレットも見守ってるよ！</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>うしプロジェクト</h1>
+    <p>by ありりん　と　とりちゃん</p>
+  </div>
+);
+
+export default Home
