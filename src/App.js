1386608b1f84f7442ded39e9eeab20b3ae0af1f5
ChieARIYOSHI <chic.720ants@gmail.com>
2021-04-22 17:28:23 +0900

    コンポーネントの追加（TopPage）_ari_20210422

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Head from './components/head.js';
+import TopPage from './components/toppage.js';
 
 const App = () => {
   return (
     <div>
       <Head />
+      <TopPage />
     </div>
   );
 }
-
 export default App;
